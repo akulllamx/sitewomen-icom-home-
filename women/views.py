d060fe2dac614e73c7e55889d20d60afb950dc33
Maxim(icom) <akulllamx@gmail.com>
2025-09-29 16:31:26 +0400

    29.09.2025 16:30

--- women/views.py
+++ women/views.py
@@ -3,46 +3,31 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.template.loader import render_to_string
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
 
 
-class MyClass:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулии Робертс', 'is_published': True},
+]
 
 
 def index(request): # HttpRequest
-    # t = render_to_string('women/index.html')
-    # return HttpResponse(t)
     data = {
         'title': 'Главная страница',
         'menu': menu,
-        'float': 28.56,
-        'lst': [1, 2, 'abc', True],
-        'set': {1, 2, 3, 2, 5},
-        'dict': {'key1': 'value1', 'key2': 'value2'},
-        'obj': MyClass(10, 20),
+        'posts': data_db,
     }
     return render(request, 'women/index.html', context=data)
 
 
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
-
-
-def categories_by_slug(request, cat_slug):
-    if request.POST:
-        print(request.POST)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
-
-
-def archive(request, year):
-    if year > 2023:
-        uri = reverse('cats', args=('sport',))
-        return HttpResponsePermanentRedirect(uri)
-
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
 
 
 def page_not_found(request, exception):
@@ -55,3 +40,15 @@ def about(request):
         'menu': menu,
     }
     return render(request, 'women/about.html', context=data)
+
+
+def addpage(request):
+    return HttpResponse(f"Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse(f"Обратная связь")
+
+
+def login(request):
+    return HttpResponse(f"Авторизация")
